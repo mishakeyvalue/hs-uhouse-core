f41e0e789a0d135d19a3a6995da620b3e4fb0a61
Mikhail Kilianovski <mitutee@gmail.com>
2018-03-21 09:25:43 +0200

    Implement serving temperature

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,70 @@
+
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE TypeOperators     #-}
+
 module Main where
 
-import Lib
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Maybe (catMaybes, isJust, listToMaybe, mapMaybe)
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
+import Servant
+import Servant.API.ResponseHeaders
+import Servant.JS
+import Text.Blaze.Html.Renderer.String
+
+
+
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as B
+
+
+
+data RawTemperature = RawTemperature { getRawTemp :: String } deriving (Show, Read)
+
+instance ToJSON TempData
+data TempData = TempData {
+        time          :: Int
+        , temperature :: Int
+    } deriving (Show, Read, Generic)
+
+type MyApi = "temperature" :> Get '[JSON] [TempData]
+
+
+myAPI :: Proxy MyApi
+myAPI = Proxy
+
+
+
+server1 :: Server MyApi
+server1 = serveTemp
+
+serveTemp :: Handler [TempData]
+serveTemp = liftIO $ (map mapTemp) <$> readTemp
+
+mapTemp :: (UTCTime, RawTemperature) -> TempData
+mapTemp (tim, (RawTemperature tem)) = TempData (round . utcTimeToPOSIXSeconds $ tim) (read tem)
+
+filePath = "/home/mitutee/temperature.ds18s20.log"
+
+readTemp :: IO  [(UTCTime, RawTemperature)]
+readTemp = catMaybes . (map (\(x, y) -> (x,) <$> y)) . (mapMaybe maybeRead) . lines <$>readFile filePath
+
+maybeRead :: Read a =>  String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
+
+app1 :: Application
+app1 = serve myAPI server1
 
 main :: IO ()
-main = someFunc
+main = run 8081 app1
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- import Codec.Serialise
+-- import Control.Exception.Base (bracket)
+-- import Data.ByteString (ByteString)
+-- import Data.Default (def)
+-- import GHC.Generics
+
+-- import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Lazy as BSL
+-- import qualified Database.RocksDB as Rocks
+
+-- data TimeBinding = LightBinding { lightId :: Int, fromTime :: Int, toTime :: Int} deriving (Show, Generic)
+
+-- instance Serialise TimeBinding where
+
+
+
+-- toStrict :: BSL.ByteString -> ByteString
+-- toStrict = BS.concat . BSL.toChunks
+
+-- withRocksDB = withRocksDB' dbPath
+
+-- sampleBinding = LightBinding 12 155 166
+
+-- dbPath :: FilePath
+-- dbPath = "_mydb"
+
+-- withRocksDB' :: FilePath -> (Rocks.DB -> IO c) -> IO c
+-- withRocksDB' path = bracket (openDB path) Rocks.close
+
+-- openDB path = Rocks.open path def { Rocks.createIfMissing = True }
